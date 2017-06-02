5fcc6e2c5aebbb95b5012ddeccadcb35b12d979f
thethego <thethe71@hotmail.fr>
2017-06-02 17:16:20 +0200

    reset number of turtles at 100

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -7,7 +7,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
  * Created by hagoterio on 26/04/17.
  */
 public class Model  extends Observable {
-    private final int INITIAL_NB_TURTLE = 40;
+    private final int INITIAL_NB_TURTLE = 100;
 
     private Turtle currentTurtle;
     private CopyOnWriteArrayList<Turtle> turtles;
